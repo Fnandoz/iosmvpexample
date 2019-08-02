d0048d64fe47b9aa6153ad755abce2ae5a41d2aa
Fernando Gomes <lfgsnando@gmail.com>
2019-08-02 20:10:13 -0300

    Initial Commit

---
+++ RegistrationPageMVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RegistrationPageMVP
+//
+//  Created by Fernando Gomes on 02/08/19.
+//  Copyright © 2019 Fernando Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
